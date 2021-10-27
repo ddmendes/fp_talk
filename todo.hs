095ad813dc739302a49c661ce403138e332f669f
Davi Mendes <d.mendes@sidi.org.br>
2021-10-27 10:18:22 -0300

    Implement mark item as done

--- todo.hs
+++ todo.hs
@@ -3,6 +3,23 @@ module Main where
 type Item = String
 type Items = [Item]
 
+data Command
+    = Quit
+    | DisplayItems
+    | AddItem String
+    | Done Int
+
+parseCommand :: String -> Either String Command
+parseCommand line = case words line of
+    ["quit"] -> Right Quit
+    ["items"] -> Right DisplayItems
+    ["done", idxStr] ->
+        if all (\c -> elem c "0123456789") idxStr
+            then Right (Done (read idxStr))
+            else Left "Invalid index."
+    "add" : "-" : item -> Right (AddItem (unwords item))
+    _ -> Left "Unknown command."
+
 -- Returns a new list of Items with the new item in it
 addItem :: Item -> Items -> Items
 addItem item items = item : items
@@ -18,20 +35,54 @@ displayItems items =
         unlines displayedItemsList
 
 -- returns a new list of items or an error essage if the index is out of bounds
--- removeItem :: Int -> Items -> Either String Items
+removeItem :: Int -> Items -> Either String Items
+removeItem reversedIndex allItems =
+        impl (length allItems - reversedIndex) allItems
+    where
+        impl index items =
+            case (index, items) of
+                (0, item : rest) -> Right rest
+                (n, []) -> Left "Index out of bounds."
+                (n, item : rest) ->
+                    case impl (n-1) rest of
+                        Right newItems -> Right (item : newItems)
+                        Left errMsg -> Left errMsg
+
 
 -- Takes a list of items
 -- Interact with the user
--- Return an updated list of items
-interactWithUser :: Items -> IO Items
+interactWithUser :: Items -> IO ()
 interactWithUser items = do
-    putStrLn "Enter an item to add to your todo list:"
-    item <- getLine
-    let newItems = addItem item items
-    putStrLn "Item added.\n"
-    putStrLn "TODO Items:"
-    putStrLn (displayItems newItems)
-    pure newItems
+    putStrLn "Commands: quit, items, add - <item to add>, done <item index>"
+    line <- getLine
+    case parseCommand line of 
+        Right DisplayItems -> do
+            putStrLn "TODO items:"
+            putStrLn (displayItems items)
+            interactWithUser items
+
+        Right (AddItem item) -> do
+            let newItems = addItem item items
+            putStrLn "Item added."
+            interactWithUser newItems
+
+        Right (Done index) -> do
+            let result = removeItem index items
+            case result of
+                Left errMsg -> do
+                    putStrLn ("Error: " ++ errMsg)
+                    interactWithUser items
+                Right newItems -> do
+                    putStrLn "Item done."
+                    interactWithUser newItems
+
+        Right Quit -> do
+            putStrLn "Bye!"
+            pure ()
+
+        Left errMsg -> do
+            putStrLn ("Error: " ++ errMsg)
+            interactWithUser items
 
 main :: IO ()
 main = do
